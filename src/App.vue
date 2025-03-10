4a6ce78f357374810f2d0952675b71e6a327221a
hd-huangchao <77830871+hc-advokate@users.noreply.github.com>
2025-03-10 17:25:49 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,56 @@
 <script setup>
 	import WOW from "wow.js";
+	import FontFaceObserver from "fontfaceobserver";
+	import { ref, reactive } from "vue";
+	// 或者在非模块化环境中，手动包含到项目里
+	// （这里省略具体HTML引用步骤）
+	let isLoadFont = ref(false);
+	const data = reactive({
+		isLoadFont: [false, false],
+	});
+	const font1 = new FontFaceObserver("Inter-Black");
+	const font2 = new FontFaceObserver("Dreamscape");
+	const font3 = new FontFaceObserver("SFPro");
+	onMounted(() => {
+		font1
+			.load()
+			.then(() => {
+				console.log("Inter-Black字体已成功加载！");
+				isLoadFont.value = true;
+				data.isLoadFont[0] = true;
+			})
+			.catch(() => {
+				console.log("Inter-Black字体加载失败。");
+				isLoadFont.value = true;
+				data.isLoadFont[0] = true;
+			});
+		font2
+			.load()
+			.then(() => {
+				console.log("Dreamscape字体已成功加载！");
+				isLoadFont.value = true;
+				data.isLoadFont[1] = true;
+			})
+			.catch(() => {
+				console.log("Dreamscape字体加载失败。");
+				isLoadFont.value = true;
+				data.isLoadFont[1] = true;
+			});
+		font3
+			.load()
+			.then(() => {
+				console.log("字体已成功加载！");
+				isLoadFont.value = true;
+			})
+			.catch(() => {
+				console.log("字体加载失败。");
+				isLoadFont.value = true;
+			});
+	});
+
+	const isLoadFontAll = computed(() => {
+		return data.isLoadFont[0] && data.isLoadFont[1];
+	});
 
 	onMounted(() => {
 		nextTick(() => {
