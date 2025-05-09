d98b58cb73c38df9fe67a4987c47043a925e9491
hc precog <hcprecog@gmail.com>
2025-05-09 16:44:00 +0800

    3

--- src/plugins/constants/index.js
+++ src/plugins/constants/index.js
@@ -8,23 +8,27 @@ import IcoRight from "@/assets/images/ico/right.png";
 import IcoDctRight from "@/assets/images/ico/direction-right.png";
 
 import HomeBg from "@/assets/images/home/bg.png";
+import HomeVideoBg from "@/assets/images/home/bg.mp4";
 import HomeImg1 from "@/assets/images/home/img1.png";
 import HomeImg2 from "@/assets/images/home/img2.png";
 import HomeImg3 from "@/assets/images/home/img3.png";
 export default {
 	logo: Logo,
-  ico: {
-    menu: IcoMenu,
-    line: IcoLine,
-    lineSel: IcoLineSel,
-    left: IcoLeft,
-    right: IcoRight,
-    dctRight: IcoDctRight,
-  },
-  home: {
-    bg: HomeBg,
-    img1: HomeImg1,
-    img2: HomeImg2,
-    img3: HomeImg3,
-  },
+	ico: {
+		menu: IcoMenu,
+		line: IcoLine,
+		lineSel: IcoLineSel,
+		left: IcoLeft,
+		right: IcoRight,
+		dctRight: IcoDctRight,
+	},
+	home: {
+		bg: HomeBg,
+		img1: HomeImg1,
+		img2: HomeImg2,
+		img3: HomeImg3,
+	},
+	video: {
+		homeBg: HomeVideoBg,
+	},
 };
